486980e3a97fa602dffa54e80efc5f2ce2821b25
Sajees XPS <sajees.jeya@gmail.com>
2024-06-24 00:03:52 -0400

    dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV !== 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.render(
   <React.StrictMode>
